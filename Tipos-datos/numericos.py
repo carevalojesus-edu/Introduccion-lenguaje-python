037eea2581442424728df205e8acc93be0c84019
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-08-23 21:05:06 -0500

    se agrego el tipo de dato numerico

---
+++ Tipos-datos/numericos.py
@@ -0,0 +1,18 @@
+# Enteros(int) los numeros sin el punto decimal
+
+entero = 20
+edadPersona = 18
+numeroCasa = 300
+alturaPersona = 187
+pesoPersona = 84
+cantidadCasas = 150
+
+
+# Flotante(float) los numeros con punto decimal, para representar las fracciones
+
+flotante = 10.5
+pi = 3.14151692
+alturaPersona = 1.87
+porcentajeBateria = 0.3
+
+print("Tu altura es", alturaPersona, "metros")
