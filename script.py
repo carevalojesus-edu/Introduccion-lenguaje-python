ae6cb807ec65b17efee331ec4329068c1545a7ef
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-08-23 20:30:43 -0500

    mi primer commit

---
+++ script.py
@@ -0,0 +1,17 @@
+# Imprimir en la consola de python
+print("Hola bienvenidos al curso de Python")
+
+nombresPersonas = ["Juan", "Luis", "Christian"]
+nombrePersona = "Christian"
+edadPersona = [12,16,31]
+
+print(nombrePersona)
+print(edadPersona)
+
+# variables
+# letras
+# numeros
+# _
+
+# nunca deben comenzar con "_"
+
