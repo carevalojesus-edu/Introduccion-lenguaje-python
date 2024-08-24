4c2e939161dc88f4cd6eaa7905f2152dcd386d4c
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-08-23 20:41:14 -0500

    se agrego la variable fecha de nacimiento

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ print("Hola bienvenidos al curso de Python")
 nombresPersonas = ["Juan", "Luis", "Christian"]
 nombrePersona = "Christian"
 edadPersona = [12,16,31]
+fechaNacimiento = "10-12-1992"
 
 print(nombrePersona)
 print(edadPersona)
