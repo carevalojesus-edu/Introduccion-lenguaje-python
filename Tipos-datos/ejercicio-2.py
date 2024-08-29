2f95ff1422802e5a2fb5e1ceddaeb5165e8f9605
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-08-28 20:18:07 -0500

    se agrego el ejercicio 2

---
+++ Tipos-datos/ejercicio-2.py
@@ -0,0 +1,29 @@
+# Declarar variables entera y convertirlas a flotantes
+# Declarar varibale flotante y convertirla e entera
+# Convertir un numero a una cadena de texto y combinarlo con otra cadena
+# Convertit una cadena de texto que represente en un entero
+
+# Convertir de entero a flotante (float)
+entero = 112
+flotante = float(entero)
+print("Convertido a flotante", flotante)
+
+# Convertir de flotante a entero (int)
+flotante = 80.9
+entero = int(flotante)
+print("Convertido a entero", entero)
+
+# Convertir de numero a una cadena (str)
+# (type) para saber que tipo de datos es
+
+numero = 123
+print(type(numero))
+cadena = str(numero)
+print("Numero convertido a cadena", cadena)
+print(type(cadena))
+
+# Convertir de cadena a entero
+cadena_numerica = "10095"
+entero = int(cadena_numerica)
+print("Cadena convertida a entero", entero)
+print(type(entero))
