b21d8f579589f1c702da470160033d70da39430b
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-08-28 21:28:37 -0500

    Se agrego las estructuras de control, condicionales

---
+++ Estructuras-control/Condicionales/if.py
@@ -0,0 +1,20 @@
+# Ejecutar instrucciones si una condiciones es VERDADERA y otra FALSA
+# Sentencia if
+
+x = 1
+
+if x > 5:
+    print("x es mayor que 5")
+else:
+    print("x es menor que 5")
+
+y = 5
+
+if y > 5:
+    print("y es mayor que 5") 
+elif y == 5:
+    print("y es igual a 5")
+elif y == 3:
+    print("y es igual a 3")
+else:
+    print("y es menor que 5")
