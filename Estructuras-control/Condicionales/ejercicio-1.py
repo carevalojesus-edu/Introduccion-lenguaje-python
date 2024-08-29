3a95d24be98c3148e2f47bee3f237620cb92d127
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-08-28 21:36:04 -0500

    Se agrego el ejercicio 1 de condicionales

---
+++ Estructuras-control/Condicionales/ejercicio-1.py
@@ -0,0 +1,14 @@
+# Ejercicio de clasificacion de numeros
+
+# Declarar un variable entero
+# vamos a usar if. elif, else para clasificar como positivo, negativo o cero
+# imprimir un mensaje de acuerdo a su clasificacion
+
+entero = 0
+
+if entero > 0:
+    print("EL numero es positivo")
+elif entero < 0:
+    print("El numero es negativo")
+else:
+    print("El numero es cero")
