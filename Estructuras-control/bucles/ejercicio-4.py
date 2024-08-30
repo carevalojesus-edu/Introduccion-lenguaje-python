c024264c42aae340db6a47e79441feba375de10c
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-08-29 21:06:18 -0500

    Se agrego el ejercicio 4 de bucles

---
+++ Estructuras-control/bucles/ejercicio-4.py
@@ -0,0 +1,14 @@
+# Crear la tabla de multiplicar, 5, 8 ,9 y 12
+
+for i in range (0,13):
+    print("5 x", i , "=", 5 * i)
+
+for i in range (0,13):
+    print("8 x", i , "=", 8 * i)
+
+
+for i in range(0,13):
+    print("9 X",i,"=",9*i)
+
+for i in range(0,13):
+    print("12 X",i,"=",12*i)
