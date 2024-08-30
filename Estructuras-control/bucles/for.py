abfdc137cd469e324f9e1c95e5cdacae1b0d6d43
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-08-29 19:50:58 -0500

    Se agrego el bucle for

---
+++ Estructuras-control/bucles/for.py
@@ -0,0 +1,27 @@
+# El bucle se utiliza para la iteracion una secuencia (lista, una tupla, una cadena, rango de numeros)
+# for variable in secuencia:
+#   Bloque de codigo a ejecutar
+
+numeros = [1,2,3,4,5,6,7,8,9] # Lista
+print(numeros)
+
+for numero in numeros:
+    print(numero)
+
+
+for i in range(5):
+    print("Numero :", i)
+
+
+cadena = "Python"
+
+for letra in cadena:
+    print(letra)
+
+
+for i in range(1,11):
+    print(i)
+
+
+for i in range(0,20,2):
+    print(i)
