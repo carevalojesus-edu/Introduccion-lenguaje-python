50ebd3af27df69ee920b761ddeec124ce5fe8f29
Christian Arevalo Jesus <christian.arevalo@unapiquitos.edu.pe>
2024-08-29 20:39:26 -0500

    Se agrego el ejercicio 2 de bucles

---
+++ Estructuras-control/bucles/ejercicio-2.py
@@ -0,0 +1,13 @@
+# Imprimir elementos de una lista
+
+frutas = ["manzana", "pera", "uva", "mandarina","naranja"]
+
+for fruta in frutas:
+    print(fruta)
+
+
+for fruta in frutas:
+    print(fruta)
+    if fruta == "uva":
+        break
+    
